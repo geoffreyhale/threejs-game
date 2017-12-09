66c5259e84d2a70bbe8633ece8262e70ae450de5
geoffreyhale <geoffreyhale@gmail.com>
2017-12-08 18:00:23 -0800

    add movement speeds

---
+++ js/move.js
@@ -0,0 +1,53 @@
+const move = (camera, keysDown) => {
+    const RESET = {
+        position: {
+            x: 0,
+            y: 0,
+            z: 5
+        },
+        rotation: {
+            y: 0
+        }
+    };
+
+    const SPEED = {
+        VERTICAL: 0.1,
+        STRAFE: 0.1,
+        FORWARD: 0.1,
+        BACKWARD: 0.05,
+        ROTATION: 0.02,
+    };
+
+    keysDown.forEach((key) => {
+        if (key === KEYS.W) {
+            camera.position.y += SPEED.VERTICAL;
+        }
+        if (key === KEYS.S) {
+            camera.position.y -= SPEED.VERTICAL;
+        }
+        if (key === KEYS.A) {
+            camera.position.x -= SPEED.STRAFE;
+        }
+        if (key === KEYS.D) {
+            camera.position.x += SPEED.STRAFE;
+        }
+        if (key === KEYS.R) {
+            camera.position.x = RESET.position.x;
+            camera.position.y = RESET.position.y;
+            camera.position.z = RESET.position.z;
+            camera.rotation.y = RESET.rotation.y;
+        }
+        if (key === KEYS.UP) {
+            camera.position.z -= SPEED.FORWARD;
+        }
+        if (key === KEYS.DOWN) {
+            camera.position.z += SPEED.BACKWARD;
+        }
+        if (key === KEYS.LEFT) {
+            camera.rotation.y += SPEED.ROTATION;
+        }
+        if (key === KEYS.RIGHT) {
+            camera.rotation.y -= SPEED.ROTATION;
+        }
+    });
+};
