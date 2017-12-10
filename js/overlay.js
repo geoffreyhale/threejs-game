66556aa997006ad976f87484b4d75f30127fc567
geoffreyhale <geoffreyhale@gmail.com>
2017-12-10 13:56:37 -0800

    improve overlay

--- js/overlay.js
+++ js/overlay.js
@@ -1,5 +1,16 @@
-const Overlay = (function() {
+const Overlay = (function () {
     let el = {};
+
+    const round = (number, precision) => {
+        const factor = Math.pow(10, precision);
+        const tempNumber = number * factor;
+        const roundedTempNumber = Math.round(tempNumber);
+        return roundedTempNumber / factor;
+    };
+
+    const radiansToDegrees = (rads) => {
+        return round(Math.abs(rads * (180 / Math.PI) % 360), 1);
+    };
 
     return {
         init: () => {
@@ -7,7 +18,11 @@ const Overlay = (function() {
         },
 
         update: (camera) => {
-            el.innerHTML = "XYZ " + camera.position.x + " " + camera.position.y + " " + camera.position.z;
+            el.innerHTML =
+                "POS XYZ: " + round(camera.position.x, 1) + " " + round(camera.position.y, 1) + " " + round(camera.position.z, 1) + "<br>"
+                + "ROT XYZ: " + round(camera.rotation.x, 1) + " " + round(camera.rotation.y, 1) + " " + round(camera.rotation.z, 1)
+                + " (" + radiansToDegrees(camera.rotation.z) + "°)"
+            ;
         },
     };
 }());
